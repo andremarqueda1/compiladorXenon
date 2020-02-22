12577fa5b7ba66db107a073080d2bbe689a2976e
andremarqueda1 <andremarqueda1@gmail.com>
2020-02-22 14:10:53 -0600

    Leer archivos

--- lib/compilador.ex
+++ lib/compilador.ex
@@ -1,25 +1,15 @@
 defmodule Compilador do
   @spec main(any) :: :ok
   def main(_args) do
-    IO.puts ("Hola!")
-    suma()
-    resta()
-    IO.puts ("Papi")
+    IO.puts ("Initiating process, reading sourceCode.c")
+    #Leyendo el archivo
+    sourceRaw=File.read!("sourceCode.c")
+    IO.puts("The read source code is:\n" <> sourceRaw)
+    sourceSplit=String.split(sourceRaw," ")
+    IO.puts (sourceSplit)
   end
 
-  def suma do
-    variable1=5
-    variable2=9
-    variable3=variable1+variable2
-    IO.puts (variable3)
-  end
-
-  def resta do
-    var1=4
-    var2=5
-    var3=var1-var2
-    IO.puts (var3)
-
+  def lector() do
 
   end
 end
