58845259d0dd9a7afe499320e99a1e677b9d7e8a
andremarqueda1 <andremarqueda1@gmail.com>
2020-02-22 16:48:09 -0600

    comentarios para proxima sesion y separacion de tokens posterior

--- lib/compilador.ex
+++ lib/compilador.ex
@@ -3,10 +3,10 @@ defmodule Compilador do
   def main(_args) do
     IO.puts ("Initiating process, reading sourceCode.c")
     IO.puts("Returning single characters for comparission\n")
-    caracteresSeparados=Scanner.lectorSeparadorUnitario()
+    caracteresSeparados=Scanner.lectorSeparadorUnitario() #Para comparar con los caracteres permitidos por C
     Enum.each(caracteresSeparados, fn x -> IO.puts (x) end)
     IO.puts("Returning filtered tokens\n")
-    tokensSeparados=Scanner.lectorSeparadorPorTokens
+    tokensSeparados=Scanner.lectorSeparadorPorTokens #Falta separar los tokens (),{},;
     Enum.each(tokensSeparados, fn x -> IO.puts (x) end)
   end
 end
